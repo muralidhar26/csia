91b35865d71af5630da820ce6677ff8561368747
muralidhar26 <muralidhar26@gmail.com>
2018-04-29 21:25:54 -0500

    First commit for csia

---
+++ components/csia-exercises/src/main/java/com/exercises/csia/ch01_2/LeapYear.java
@@ -0,0 +1,19 @@
+package com.exercises.csia.ch01_2;
+
+public class LeapYear {
+    public static void main(String[] args) { 
+        int year = Integer.parseInt(args[0]);
+        boolean isLeapYear;
+
+        // divisible by 4
+        isLeapYear = (year % 4 == 0);
+
+        // divisible by 4 and not 100
+        isLeapYear = isLeapYear && (year % 100 != 0);
+
+        // divisible by 4 and not 100 unless divisible by 400
+        isLeapYear = isLeapYear || (year % 400 == 0);
+
+        System.out.println("The given year: "+year+" is a leap year: "+isLeapYear);
+    }
+}
