9c712c9ec1d7b1e4477bc587d01f09b82a60d036
muralidhar26 <muralidhar26@gmail.com>
2018-11-21 22:24:47 +0530

    added Ex1.2.9 and Ex1.2.10

---
+++ components/csia-exercises/src/main/java/com/exercises/csia/ch01_2/Ex_1_2_9.java
@@ -0,0 +1,12 @@
+package com.exercises.csia.ch01_2;
+
+public class Ex_1_2_9 {
+
+    public static void main(String[] args){
+
+        System.out.println('b');
+        System.out.println('b'+'c');
+        System.out.println((char)'a'+4);
+
+    }
+}
