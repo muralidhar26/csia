0d643f90760c47e6753870e89da4624144846dfa
muralidhar26 <muralidhar26@gmail.com>
2018-11-21 22:31:36 +0530

    added Ex1.2.11

---
+++ components/csia-exercises/src/main/java/com/exercises/csia/ch01_2/Ex_1_2_11.java
@@ -0,0 +1,15 @@
+package com.exercises.csia.ch01_2;
+
+public class Ex_1_2_11 {
+
+    public static void main(String[] args){
+
+        double a = 3.14159;
+        System.out.println(a);
+        System.out.println(a+1);
+        System.out.println(8/(int)a);
+        System.out.println(8/a);
+        System.out.println((int) (8/a));
+
+    }
+}
