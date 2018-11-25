68b3aec37616667425904a4a835a0d715855906d
muralidhar26 <muralidhar26@gmail.com>
2018-11-25 18:27:53 +0530

    added WindChill

---
+++ components/csia-exercises/src/main/java/com/exercises/csia/ch01_2/WindChill.java
@@ -0,0 +1,13 @@
+package com.exercises.csia.ch01_2;
+
+public class WindChill {
+
+    public static void main(String[] args){
+        double temperature = Double.parseDouble(args[0]);
+        double velocity = Double.parseDouble(args[1]);
+
+        double windChill = 35.74 + 0.6215 * temperature + (0.4275*temperature - 35.75)*Math.pow(velocity, 0.16);
+
+        System.out.println("Wind Chill is "+windChill);
+    }
+}
