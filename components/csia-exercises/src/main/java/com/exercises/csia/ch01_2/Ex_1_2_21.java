d8439a23af824b861544d59b6bd16bae9857b32a
muralidhar26 <muralidhar26@gmail.com>
2018-11-25 12:28:06 +0530

    added Ex_1_2_21

---
+++ components/csia-exercises/src/main/java/com/exercises/csia/ch01_2/Ex_1_2_21.java
@@ -0,0 +1,11 @@
+package com.exercises.csia.ch01_2;
+
+public class Ex_1_2_21 {
+
+    public static void main(String[] args){
+         double theta = Double.parseDouble(args[0]);
+
+         double value = Math.sin(2*theta) + Math.sin(3*theta);
+         System.out.println("Value is "+value);
+    }
+}
